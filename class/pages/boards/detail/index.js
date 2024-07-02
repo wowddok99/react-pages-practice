7b69c94f9a7e1f5a834a82efd8ec680444615535
전상은 <wowddok99@gmail.com>
2024-07-02 20:53:27 +0900

    boards/detail

---
+++ class/pages/boards/detail/index.js
@@ -0,0 +1,8 @@
+import {} from "../../../styles/boardsDetail"
+
+export default function BoardsDetailPage(){
+
+    return (
+        <div>detailPage 입니다.</div>
+    )
+}
---
+++ class/styles/boardsDetail.js
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const Wrraper = styled.div`
+    font-family: "NotoSansKR-Regular";
+    width: 1200px;
+    border: 1px solid black;
+    margin: 100px;
+    padding-top: 90px;
+    padding-bottom: 100px;
+    padding-left: 100px;
+    padding-right: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: none;
+    box-shadow: 0px 0px 10px gray;
+`
+
